423ca0eb93d91a880ab6beb0411b6f3542f8d0f7
Lukiwoki <LukiwokiMC@gmail.com>
2018-06-25 21:11:03 +0200

    Changed stuff

--- src/us/sunset/punishment/PunishCommand.java
+++ src/us/sunset/punishment/PunishCommand.java
@@ -15,76 +15,11 @@ public class PunishCommand implements CommandExecutor
 	//punish <Player> <Offence:Severity> <Reason>
 	
 	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) 
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
 	{
-		
-		if (!sender.hasPermission("sunset.punish"))
-		{
-			return false;
-		}
-		
-		if (args.length <= 0)
-		{
-			sender.sendMessage("something something too not enough arguments");
-			return false;
-		}
-		
-		String reason = null;
-		OfflinePlayer target = Bukkit.getOfflinePlayer(args[0]);
-		
-		if (target.equals(null))
-		{
-			sender.sendMessage("player doesn't exist or something like that");
-			return false;
-		}
-		
-		if (args.length > 2)
-		{
-			StringBuilder sb = new StringBuilder();
-			for (int i = 2; i < args.length; i++)
-			{
-				sb.append(args[i]+" ");
-			}
-			
-			reason = sb.toString();
-		}
-		
-		Punishments punishment = Punishments.valueOf(args[0].toUpperCase());
-		
-		
-		
-		try
-		{
-			Punishments.valueOf(args[0].toUpperCase());
-		}
-		catch(Exception e)
-		{
-			
-		}
-		
-		
-		String os = args[0];
-		
-		if (args[0].contains(":"))
-		{
-			String[] osl = args[0].split(":");
-			os = osl[0];
-		}
-		
-		try
-		{
-			Punishments.valueOf(os.toUpperCase());
-		}
-		catch(Exception e)
-		{
-			sender.sendMessage("Punishment doesn't exist something something");
-		}
-		
-		
-		
-		new PunishGUI().open((Player)sender);
-		
-		
+
+		//Leaving this to JOProsper
+
 		return false;
 	}
 	
