a5562cd089a05124e037b9a0177f8dbc6e1ecaab
Lukiwoki <LukiwokiMC@gmail.com>
2018-06-26 13:19:06 +0200

    Changed punishment command

--- src/us/sunset/punishment/PunishCommand.java
+++ src/us/sunset/punishment/PunishCommand.java
@@ -1,13 +1,8 @@
 package us.sunset.punishment;
 
-import org.bukkit.Bukkit;
-import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-
-import us.sunset.gui.guis.PunishGUI;
 
 public class PunishCommand implements CommandExecutor 
 {
